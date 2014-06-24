805c190a3699a9521d51d59de53b123dbf606cb0
Dion Hutchings <dionhut@outlook.com>
2014-06-24 13:00:41 -0700

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+
+name "kibana3"
+description "Installs Kibana3"
+maintainer "DionHut"
+maintainer_email "dionhut@outlook.com"
+depends "nginx"
+depends "ark"
+supports "ubuntu"
+version "1.0.0"
---
+++ recipes/default.rb
@@ -0,0 +1,40 @@
+
+directory node['kibana3']['install_dir'] do
+  recursive true
+  owner ubuntu
+  mode "0755"
+end
+
+include_recipe 'ark::default'
+
+ark 'kibana' do
+  url node['kibana3']['install_zip_url']
+  path node['kibana3']['install_dir']
+  owner ubuntu
+  action :put
+end
+
+template "#{node['kibana3']['install_dir']}/config.js" do
+  source 'config.js.erb'
+  cookbook 'kibana3'
+  mode "0750"
+  owner 'ubuntu'
+end
+
+node.set['nginx']['default_site_enabled'] = false
+
+include_recipe "nginx"
+
+template "/etc/nginx/sites-available/kibana" do
+  source 'kibana-nginx.conf.erb'
+  cookbook 'kibana'
+  notifies :reload, "service[nginx]"
+  variables(
+    :install_dir       => node['kibana']['install_dir'],
+    :listen_port      => node['kibana']['nginx']['listen_port'],
+  )
+  mode "0750"
+  owner 'ubuntu'
+end
+
+nginx_site "kibana"
