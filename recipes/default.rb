53cbd6f98f4e175d87261d013378c25832793adf
Dion Hutchings <dion.hutchings@inrix.com>
2014-06-24 22:11:01 -0700

    Updated dir chmod values

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,24 +1,24 @@
 
 directory node['kibana3']['install_dir'] do
   recursive true
-  mode "0750"
+  mode "0755"
 end
 
 include_recipe 'ark::default'
 
-ark 'kibana' do
+ark 'kibana3' do
   url node['kibana3']['install_zip_url']
   path node['kibana3']['install_dir']
   action :put
 end
 
-template "#{node['kibana3']['install_dir']}/config.js" do
+template "#{node['kibana3']['install_dir']}/kibana3/config.js" do
   source 'config.js.erb'
   cookbook 'kibana3'
   variables(
     :es_host      => node['kibana3']['es_host'],
   )
-  mode "0750"
+  mode "0755"
 end
 
 package "nginx" do
@@ -32,7 +32,7 @@ template "/etc/nginx/sites-available/default" do
     :install_dir       => node['kibana3']['install_dir'],
     :listen_port      => node['kibana3']['nginx']['listen_port'],
   )
-  mode "0750"
+  mode "0755"
 end
 
 service "nginx" do
