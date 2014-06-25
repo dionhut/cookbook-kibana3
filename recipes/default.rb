23778ab0b1e336d9f2a54984b57b0f952b1cdd85
Dion Hutchings <dion.hutchings@inrix.com>
2014-06-24 20:09:44 -0700

    Updated after debugging default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,6 @@
 
 directory node['kibana3']['install_dir'] do
   recursive true
-  owner 'ubuntu'
   mode "0750"
 end
 
@@ -10,7 +9,6 @@ include_recipe 'ark::default'
 ark 'kibana' do
   url node['kibana3']['install_zip_url']
   path node['kibana3']['install_dir']
-  owner 'ubuntu'
   action :put
 end
 
@@ -18,21 +16,22 @@ template "#{node['kibana3']['install_dir']}/config.js" do
   source 'config.js.erb'
   cookbook 'kibana3'
   mode "0750"
-  owner 'ubuntu'
 end
 
-apt_package "nginx" do
+package "nginx" do
   action :install
 end
 
 template "/etc/nginx/sites-available/default" do
   source 'kibana-nginx.conf.erb'
   cookbook 'kibana3'
-  notifies :reload, "service[nginx]"
   variables(
     :install_dir       => node['kibana3']['install_dir'],
     :listen_port      => node['kibana3']['nginx']['listen_port'],
   )
   mode "0750"
-  owner 'ubuntu'
+end
+
+service "nginx" do
+  action: restart
 end
